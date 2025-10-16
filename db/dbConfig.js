4acf3a9669e8ef9a566c25920f6c85a013b35efb
Chantorn1986 <p.chantorn1986@gmail>
2025-10-16 15:24:12 +0700

    add some fretures

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,7 +1,22 @@
 module.exports = {
+    host: "localhost",
+    user: "premier_sa",
+    password: "Premier021812299",
+    database: "pac_system",
+  };
+/*
+module.exports = {
     host: "103.30.127.30",
     user: "premier_sa",
     password: "Premier021812299",
     database: "pac_system",
     port:"3000"
   };
+
+
+  module.exports = {
+    host: "localhost",
+    user: "root",
+    password: "admin",
+    database: "pac_system"
+  };*/
